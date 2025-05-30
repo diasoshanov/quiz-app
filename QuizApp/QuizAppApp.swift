967b7710d3641f72e1e1592f7862f845c8c3193a
Dias <diasroshanov10@gmail.com>
2025-05-30 15:35:18 +0500

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Диас Рошанов on 30.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
